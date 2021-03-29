823fd997c2e4eab370f225ef12ab0ce3c5f3e077
Albert Gergo <gergoalbert.gta@gmail.com>
2021-03-29 21:30:35 +0300

    feat: cors setup

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,1 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const __prod__ = process.env.NODE_ENV === "production";
+
+export const FRONTEND_URL = __prod__
+  ? process.env.FRONTEND_URL
+  : "http://localhost:4200";
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import helmet from "helmet";
-import { __prod__ } from "./constants";
+import { FRONTEND_URL, __prod__ } from "./constants";
 import { notFound, errorHandler } from "./middlewares/error";
 import { createConnection } from "typeorm";
 import { connectionConfig } from "./ormconfig";
 import { checkAuth } from "./middlewares/auth";
 import api from "./api/";
 import passport from "passport";
+import cors from "cors";
 
 dotenv.config();
 
@@ -21,6 +22,12 @@ createConnection(connectionConfig);
 // Middleware
 app.use(passport.initialize());
 app.use(express.json());
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(morgan(__prod__ ? "common" : "dev"));
 app.use(helmet());
 app.use(checkAuth);
