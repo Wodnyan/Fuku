58d7ee050226218883538611638609e0cd894079
Albert Gergo <gergoalbert.gta@gmail.com>
2021-03-30 21:46:27 +0300

    feat: auth sign up service, sign up input validation, input error messages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LandingPageComponent } from "./views/landing-page/landing-page.component";
+import { SignUpPageComponent } from "./views/auth/sign-up-page/sign-up-page.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, LandingPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, LandingPageComponent, SignUpPageComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ frontend/src/app/views/auth/sign-up-page/sign-up-page.component.ts
@@ -0,0 +1,120 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthService } from "src/app/services/auth/auth.service";
+
+type Input = "username" | "email" | "password";
+
+interface ValidationError {
+  input: Input;
+  errors: any;
+}
+
+@Component({
+  selector: "app-sign-up-page",
+  templateUrl: "./sign-up-page.component.html",
+  styleUrls: ["./sign-up-page.component.scss"],
+  providers: [AuthService],
+})
+export class SignUpPageComponent implements OnInit {
+  credentials = new FormGroup({
+    username: new FormControl("", [
+      Validators.required,
+      Validators.maxLength(100),
+    ]),
+    email: new FormControl("", [
+      Validators.required,
+      Validators.maxLength(320),
+      // Email validation pattern
+      Validators.pattern(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      ),
+    ]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.maxLength(120),
+    ]),
+  });
+  errors: ValidationError[] = [
+    { input: "username", errors: null },
+    { input: "password", errors: null },
+    { input: "email", errors: null },
+  ];
+
+  constructor(private auth: AuthService) {}
+
+  public get username() {
+    return this.credentials.get("username");
+  }
+
+  public get email() {
+    return this.credentials.get("email");
+  }
+
+  public get password() {
+    return this.credentials.get("password");
+  }
+
+  public getErrors(input: Input): ValidationError["errors"] {
+    return this.errors.find((error) => error.input === input).errors || {};
+  }
+
+  private isErrorsEmpty() {
+    return this.errors.every((error) => error.errors === null);
+  }
+
+  public async onSubmit() {
+    this.errors = [
+      { input: "password", errors: this.password.errors },
+      { input: "username", errors: this.username.errors },
+      { input: "email", errors: this.email.errors },
+    ];
+    if (this.isErrorsEmpty()) {
+      this.auth.signUp(this.credentials.value).subscribe(
+        (data) => {
+          console.log(data);
+        },
+        ({ error }) => {
+          const isEmailInvalid = error.errors?.some((error: string) => {
+            return error.toLowerCase().includes("email");
+          });
+          if (error.status === 400 && isEmailInvalid) {
+            this.errors = this.errors.map((error) => {
+              if (error.input === "email") {
+                return {
+                  input: "email",
+                  errors: {
+                    pattern: true,
+                  },
+                };
+              }
+              return error;
+            });
+            // Username or Email taken
+          } else if (error.status === 409) {
+            if (error.message.toLowerCase().includes("email")) {
+              this.errors = this.errors.map((error) => {
+                if (error.input === "email") {
+                  error.errors = {
+                    notUnique: true,
+                  };
+                }
+                return error;
+              });
+            } else {
+              this.errors = this.errors.map((error) => {
+                if (error.input === "username") {
+                  error.errors = {
+                    notUnique: true,
+                  };
+                }
+                return error;
+              });
+            }
+          }
+        }
+      );
+    }
+  }
+
+  ngOnInit(): void {}
+}
