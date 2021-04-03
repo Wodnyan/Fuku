80c3c4c4159a337abac25d59c2e5dbd2f0031442
Albert Gergo <gergoalbert.gta@gmail.com>
2021-04-03 20:14:16 +0300

    refactor: code reviews is a subpath of rooms

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import users from "./users/users.routes";
-// import codeReviews from "./codeReviews/codeReviews.routes";
 import oauth from "./oauth/oauth.routes";
 import rooms from "./rooms/rooms.routes";
 
@@ -15,6 +14,5 @@ router.get("/", (_, res) => {
 router.use("/users", users);
 router.use("/oauth", oauth);
 router.use("/rooms", rooms);
-// router.use("/code-reviews", codeReviews);
 
 export default router;
