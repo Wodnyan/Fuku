10870063a6afd8da31b6921b7b5eac398f009c78
Albert Gergo <gergoalbert.gta@gmail.com>
2021-04-08 21:54:50 +0300

    feat: mobile friendly nav, button and anchor tag reset, close user options on click outside

--- frontend/src/app/components/nav/nav-user-options/nav-user-options.component.ts
+++ frontend/src/app/components/nav/nav-user-options/nav-user-options.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, ElementRef, Input, OnInit } from "@angular/core";
 import { User } from "src/types";
 
 @Component({
   selector: "app-nav-user-options",
   templateUrl: "./nav-user-options.component.html",
   styleUrls: ["./nav-user-options.component.scss"],
+  host: {
+    "(document:click)": "onClick($event)",
+  },
 })
 export class NavUserOptionsComponent implements OnInit {
-  public showOptions = false;
   @Input()
   user: User | null;
 
+  public showOptions = true;
+
+  constructor(private _eref: ElementRef) {}
+
+  onClick(event: MouseEvent) {
+    if (!this._eref.nativeElement.contains(event.target)) {
+      this.showOptions = false;
+    }
+  }
   ngOnInit(): void {}
 
   get avatar() {
